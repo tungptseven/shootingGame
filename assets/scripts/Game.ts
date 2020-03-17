a3cdf1e900d952b0da15afcb99f7a51995f9e91d
Tung Phan <tungpt.seven@gmail.com>
2020-03-17 10:21:32 +0700

    complete base - adding sound(wip)

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -5,18 +5,69 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator
 
 @ccclass
 export default class Game extends cc.Component {
-    spawn (event) {}
-    // LIFE-CYCLE CALLBACKS:
+    @property(cc.Prefab)
+    bullet: cc.Prefab = null
 
-    onLoad () {
-        this.node.on('mousedown', this.spawn, )
+    @property(cc.Prefab)
+    badguy: cc.Prefab = null
+
+    @property(cc.Label)
+    scoreLabel: cc.Label = null
+
+    @property
+    posX: number = 0
+
+    @property
+    posY: number = 0
+
+    @property
+    score: number = 0
+
+    // LIFE-CYCLE CALLBACKS:
+    spawn(event) {
+        let newBullet = cc.instantiate(this.bullet)
+        newBullet.setPosition(this.node.getChildByName('soldier').position.x, this.node.getChildByName('soldier').position.y)
+        this.node.addChild(newBullet)
+
+        let mousePosition = event.getLocation()
+        mousePosition = this.node.convertToNodeSpaceAR(mousePosition)
+        this.posX = mousePosition.x
+        this.posY = mousePosition.y
+
+        let actionBy = cc.moveTo(0.2, cc.v2(this.posX, this.posY))
+        let destruction = cc.callFunc(() => {
+            newBullet.destroy()
+        }, this)
+        let sequence = cc.sequence(actionBy, destruction)
+        newBullet.runAction(sequence)
     }
 
-    start () {
+    createBad() {
+        let newBadGuy = cc.instantiate(this.badguy)
+        const positions = [
+            cc.v2(-643, 382), cc.v2(570, -486), cc.v2(-627, 21), cc.v2(636, -7),
+            cc.v2(643, -382), cc.v2(-570, 486), cc.v2(627, -21), cc.v2(-636, 7),
+        ]
+        let badGuyPosition = Math.floor(Math.random() * positions.length)
+        newBadGuy.setPosition(positions[badGuyPosition])
+        this.node.addChild(newBadGuy)
+    }
+
+    addScore() {
+        this.score += 10
+        this.scoreLabel.string = 'SCORE: ' + this.score.toString()
+    }
+
+    onLoad() {
+        this.node.on('mousedown', this.spawn, this)
+        this.schedule(this.createBad, 1, cc.macro.REPEAT_FOREVER, 3)
+    }
+
+    start() {
 
     }
 
--- assets/scripts/bad.ts
+++ assets/scripts/bad.ts
@@ -5,25 +5,39 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator
 
 @ccclass
 export default class Bad extends cc.Component {
     action: cc.ActionInterval
-    
+
     // LIFE-CYCLE CALLBACKS:
+    onCollisionEnter(other, self) {
+        if (other.tag == 2) {
+            this.node.destroy()
+            this.node.parent.getComponent('Game').addScore()
+        }
+        if (other.tag == 1) {
+            cc.director.loadScene('Game')
+        }
+    }
 
     moveToPLayer() {
         let moveAction = cc.moveTo(3, this.node.parent.getChildByName('soldier').position.x, this.node.parent.getChildByName('soldier').position.y)
         return moveAction
     }
 
-    onLoad() { 
+    onLoad() {
         this.action = this.moveToPLayer()
         this.node.runAction(this.action)
+
+        let manager = cc.director.getCollisionManager()
+        manager.enabled = true
+
+        cc.director.preloadScene('Game')
     }
 
-    start () {
+    start() {
 
     }
 
